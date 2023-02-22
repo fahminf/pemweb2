55973003ccd2dcae801b6573fbf5535ada101121
fahminf <114577436+fahminf@users.noreply.github.com>
2023-02-22 15:06:07 +0700

    Delete variabel_user.php

--- variabel_user.php
+++
@@ -1,8 +0,0 @@
-<?php
-$nama = "Fahmi Nurfadillah";
-$prodi = "Sistem Informasi";
-$ipk = 4.00;
-
-echo "Nama saya adalah " . $nama . "<br>";
-echo "Program studi $prodi <br>";
-echo "IPK saya " . $ipk . ", Aamin";
